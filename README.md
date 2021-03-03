ce368dbd3692179732cd395bb35bc3606a49c07c
Sahil Limbe <sahil.d.limbe@gmail.com>
2021-03-03 14:44:16 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# SpiritWithin Flutter
+
+A new Flutter application for SpiritWithin.
+
+# Installation
+
+```
+flutter pub get
+```
+# Usage
+
+```
+flutter run
+```
---
+++ android/app/src/main/java/com/urantiatech/spirit_within_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.urantiatech.spirit_within_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
